463e6415569d2c576c834f2106c56a66b662d7c0
Atishay <atishay2024@outlook.com>
2024-12-29 15:26:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,111 @@
+# Configure AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 bucket for static website hosting
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "cloud-fusion-hello-14"
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# S3 bucket policy
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      },
+    ]
+  })
+}
+
+# EC2 Instance
+resource "aws_instance" "app_server" {
+  ami           = "ami-01816d07b1128cd2d" # Amazon Linux 2 AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServer"
+  }
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# Output values
+output "website_bucket_name" {
+  value = aws_s3_bucket.website_bucket.id
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
